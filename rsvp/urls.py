e505fa843cf0512590e09bd67f53faf245e2dba7
Thpavel <tanvirhossenpavel2018@gmail.com>
2026-03-06 17:10:34 +0530

    rsvp  bug solve again

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "rsvp"
 urlpatterns = [
     path('event/<slug:slug>/', views.rsvp_event, name='rsvp_event'),
     path('cancel/<int:rsvp_id>/', views.cancel_rsvp, name='cancel_rsvp'),
